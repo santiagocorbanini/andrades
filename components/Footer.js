1935d553d6ed228ee5a57999e46fd26c135acdde
Franco Vanney <francoavanney@gmail.com>
2022-07-06 19:27:40 -0300

    add bootstrap

--- components/Footer.js
+++ components/Footer.js
@@ -2,20 +2,33 @@ import Link from "next/link"
 import styles from "../styles/Footer.module.css"
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-        <div className={`contenedor ${styles.contenido}`}>
-            <nav className={styles.navegacion}>
-                <Link href="/">Inicio</Link>
-                <Link href="/fechas">FECHAS</Link>
-                <Link href="/media">MEDIA</Link>
-                <Link href="/bio">BIO</Link>
-                <Link href="/contacto">CONTACTO</Link>
-            </nav>
 
-            <p className={styles.copyright}>Todos los derechos reservados</p>
-        </div>
-    </footer>
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={`${styles.footer} text-center text-lg-start`}>
+    <div className="container d-flex justify-content-center py-5">
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
+      El
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
+      Nene
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
+      Esta
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
+      Bien
+      </button>
+    </div>
+
+
+    <div className="text-center text-white p-3 bg-dark">
+      © {year} Copyright:  
+      <a className="text-white" href="https://pampacode.com/">  pampacode.com</a>
+    </div>
+
+  </footer>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
